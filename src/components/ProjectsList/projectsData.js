6dba7ee3b4527963877bfb23e067f1386116471d
umutgyk95@gmail.com <ug@Umuts-MacBook-Pro.local>
2020-08-20 14:33:30 +0300

    bugs fixed

--- src/components/ProjectsList/projectsData.js
+++ src/components/ProjectsList/projectsData.js
@@ -12,7 +12,7 @@ export default [
     image: tusmatik,
     url: 'https://github.com/umutgeyik/Tusmatik',
     description:
-      'Tusmatik is an IOS/Android forum/timer app for doctors. TUS is an important exam to become a specialist doctor. There is a counter for TUS date. Additionaly, there is a forum that doctors can share their comments, thoughts and some information.'
+      'Tusmatik is an IOS/Android forum/timer app for doctors. TUS is an important step to become a specialist doctor. The counter provides an animation showing the remaining time to the TUS date. Additionaly, there is a forum where doctors can share their comments thoughts and information.'
   },
     
     {
@@ -22,7 +22,7 @@ export default [
       image: bahuzuApi,
       url: 'https://github.com/umutgeyik/bahuzuapi',
       description:
-        'Bahuzu is an online therapy app for mobile users. This api supports online payments for Bahuzu app. It uses iyzico to take the payment. It is a gateway between Bahuzu app and iyzico.'
+        'Bahuzu is an online therapy app for mobile users. This API establishes the gateway between Bahuzu APP and iyzico payment systems. Iyzico provides the necessary infrastructure for the execution of payments.'
     },
     {
       title: 'Bahuzu',
@@ -31,7 +31,7 @@ export default [
       image: img01,
       url: 'https://github.com/umutgeyik/bahuzuapi',
       description:
-        'Bahuzu is an online therapy app for psychological counseling. There are many psychologists who specialize in many fields. Users can make an appointment with our specialists for specific date and time and talk via the application. In this way, a session is completed.'
+        'Bahuzu is an online therapy app for psychological counseling. There are many psychologists who specialize in many fields. Users can make an appointment with our specialists for specific date and time. In the appointment time, users have their sessions via the application by video conferencing system.'
     },
     {
       title: 'Social Calendar',
